9d763ce649200f040d55685a1b99412519b42417
PragyanKumar <81701254+PragyanKumar@users.noreply.github.com>
2021-07-25 21:36:16 +0530

    Add files via upload

--- boi.js
+++ boi.js
@@ -1,22 +1,14 @@
 Student_slave_array=[];
 function submit(){
-    var display_array=[]
-    for(var j=1;j<=4;j++){
-        var Name1=document.getElementById("Student"+j).value;
+        var Name1=document.getElementById("Friend1").value;
         Student_slave_array.push(Name1);
         console.log(Name1)
-    }
+    
     var length1=Student_slave_array.length
     console.log(length1)
 
     console.log(Party_friend_array)
-for(var k=0;k<length1;k++){
-    display_array.push("<h4> name: "+Party_friend_array[k]+"</h4>")
-    console.log(display_array)
-}document.getElementById("no_comma_display").innerHTML=display_array
-var no_comma_array=display_array.join(" ")
-console.log(no_comma_array)
-document.getElementById("display_name").innerHTML=no_comma_array
+    document.getElementById("no_comma_display").innerHTML=Student_slave_array.toString()
 
     
     document.getElementById("SendIt").style.display="none"
@@ -26,29 +18,29 @@ function sorting(){
 Party_friend_array.sort()
 
 console.log(Party_friend_array)
-var display_array_1=[]
-var length2=Party_friend_array.length
-console.log(length2)
-for(var k=0;k<length2;k++){
-    display_array_1.push("<h4> name: "+Party_friend_array[k]+"</h4>")
-    console.log(display_array_1)
-}document.getElementById("no_comma_display").innerHTML=display_array_1
-var no_comma_array=display_array_1.join(" ")
+
+var no_comma_array=display_array_1.join("<br>")
 console.log(no_comma_array)
-document.getElementById("display_name").innerHTML=no_comma_array
+document.getElementById("display_name").innerHTML=no_comma_array.toString()
 }
 function update(){
     document.getElementById("display_name").innerHTML="<h1>"+Party_friend_array+"</h1>"
 }
-function searching(){
-    var s=document.getElementById("s1").value
-    var found=0
-    var j
-    for (j=0;j<Party_friend_array.length;j++){
-        if (s==Party_friend_array[j]){
-            found=found+1;
-        }
-    }
-}
+
 document.getElementById("p2").innerHTML="name found" +found+ "time/s"
-console.log("found name" +found+ "time/s") 
+console.log("found name" +found+ "time/s")
+
+
+function show() { var i= names_of_people.join(""); 
+console.log(names_of_people); 
+document.getElementById("p1").innerHTML=i.toString(); 
+document.getElementById("sort_button").style.display="block"; 
+} 
+function searching() { var s= document.getElementById("s1").value;
+ var found=0; 
+ var j;
+  for(j=0; j<Party_friend_array.length;j++) 
+  { if(s==Party_friend_array[j]){ found=found+1; } 
+} document.getElementById("p2").innerHTML="name found "+found+" time/s"; 
+console.log("found name "+found+" time/s"); 
+}
